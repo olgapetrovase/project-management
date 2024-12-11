c52602c9d325ff9d4b5d4c4c9e0458381442a120
Olha Petrova <olga.petrova.se@gmail.com>
2024-12-10 19:12:09 -0800

    Delete project functionality

--- src/App.jsx
+++ src/App.jsx
@@ -51,11 +51,24 @@ function App() {
       };
     });
   }
+
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      let updatedProjects = prevState.projects
+            .filter(project => project.id !== prevState.selectedProjectId);
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: updatedProjects
+      };
+    });
+  }
   
   const selectedProject = projectsState.projects
       .find(project => project.id === projectsState.selectedProjectId);
   
-    let content = <SelectedProject project={selectedProject} />;
+    let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
